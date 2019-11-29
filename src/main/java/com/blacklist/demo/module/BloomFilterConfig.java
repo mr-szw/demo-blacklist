2b77a2970b6e2cd72244015458e0e2c715d046c0
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2019-11-29 19:42:23 +0800

    add config class

---
+++ src/main/java/com/blacklist/demo/module/BloomFilterConfig.java
@@ -0,0 +1,17 @@
+package com.blacklist.demo.module;
+
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * Create by Dawei on 2019/11/29
+ */
+@Data
+public class BloomFilterConfig {
+
+	//
+	private String filterName;
+
+	private String filterDesc;
+
+}
